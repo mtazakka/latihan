4649dc7e4200140bc9067f4b958cad0ae41232ca
Muhammad Tazakka Ramadhan <108384217+mtazakka@users.noreply.github.com>
2022-10-14 19:26:50 +0700

    test

---
+++ cobaapi/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+module.exports = function (db) {
+  router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+
+  return router;
+}
