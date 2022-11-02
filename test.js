7aa90f70280ba2889ce2ee5546dc7c61840901b4
Muhammad Tazakka Ramadhan <108384217+mtazakka@users.noreply.github.com>
2022-11-02 20:31:41 +0700

    tes 2

--- test.js
+++ test.js
@@ -1,23 +1,13 @@
-let nums = [3, 2, 3];
-let target = 6;
-let result = [];
-
-var twoSum = function (nums, target) {
-    var map = {};
-    for (var i = 0; i < nums.length; i++) {
-        var n = nums[i];
-        // console.log("n=", n)
-
-        if (map[target - n] >= 0) {
-            return [map[target - n], i]
-        }
-        else {
-            map[n] = i;   //use map to store the value and index position
-            console.log(map[n] + '=' + i)
-        }
+class Mobil {
+    constructor(door, tyre) {
+        this.door = door
+        this.tye = tyre
     }
-};
+    go() {
+        return `Berangkat ${this.door}`
+    }
+}
 
-console.log(twoSum(nums, target))
-ok
-console.log(1)
+const car = new Mobil(4, 5)
+
+console.log(car.go())
